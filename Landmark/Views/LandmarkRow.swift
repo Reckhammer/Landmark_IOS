95ad87bad17867f571a44a9be9ce803f1c694d3e
Reckhammer <joshua.rechkemmer48@gmail.com>
2024-02-01 17:58:38 -0600

    Finished Chapter 2 Section 2

---
+++ Landmark/Views/LandmarkRow.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkRow.swift
+//  Landmark
+//
+//  Created by Joshua Rechkemmer on 2/1/24.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkRow()
+}
