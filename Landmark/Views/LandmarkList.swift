7c95d2caecd7f792e979a11d2faeb0975a3d2481
Reckhammer <joshua.rechkemmer48@gmail.com>
2024-02-01 18:10:12 -0600

    Finished Chapter 2 Section 5: created a dynamic landmarks list

--- Landmark/Views/LandmarkList.swift
+++ Landmark/Views/LandmarkList.swift
@@ -7,9 +7,14 @@
 
 import SwiftUI
 
-struct LandmarkList: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct LandmarkList: View 
+{
+    var body: some View 
+    {
+        List(landmarks)
+        {
+            landmark in LandmarkRow(landmark: landmark);
+        }
     }
 }
 
