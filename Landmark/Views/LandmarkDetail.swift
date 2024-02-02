1004957862e74bbab57eab9a3b260834be1c5ff4
Reckhammer <joshua.rechkemmer48@gmail.com>
2024-02-01 18:17:42 -0600

    Finished Chapter 2 Section 6: set up navigatio between list and detail

---
+++ Landmark/Views/LandmarkDetail.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkDetail.swift
+//  Landmark
+//
+//  Created by Joshua Rechkemmer on 2/1/24.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
