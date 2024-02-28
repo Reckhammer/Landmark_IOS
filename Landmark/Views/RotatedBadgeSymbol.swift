c08a7512b65e0224201a7868717b4cc3d43c4b42
Reckhammer <joshua.rechkemmer48@gmail.com>
2024-02-28 17:00:52 -0600

    Creating the badge symbol, Chapter 2 Section 3

---
+++ Landmark/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,18 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmark
+//
+//  Created by Joshua Rechkemmer on 2/28/24.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol()
+}
