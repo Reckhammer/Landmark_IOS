edb6d8b4c5974f778a691713e5f873ed14c1978a
Reckhammer <joshua.rechkemmer48@gmail.com>
2024-03-07 17:45:05 -0600

    Chapter 2 Section 4, combined the badge foreground and background

---
+++ Landmark/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmark
+//
+//  Created by Joshua Rechkemmer on 2/28/24.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View 
+{
+    let angle: Angle;
+    
+    var body: some View
+    {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom);
+    }
+}
+
+#Preview 
+{
+    RotatedBadgeSymbol(angle: Angle(degrees:  5));
+}
