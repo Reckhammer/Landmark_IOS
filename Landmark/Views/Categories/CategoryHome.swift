73c1120ab8ee693efb70a44bd5d32f048f10010b
Reckhammer <joshua.rechkemmer48@gmail.com>
2024-03-07 18:45:54 -0600

    Chapter 3 Part 1 Section 4, Added a isFeatured section and changed how imaged are displayed in category home

--- Landmark/Views/Categories/CategoryHome.swift
+++ Landmark/Views/Categories/CategoryHome.swift
@@ -17,13 +17,22 @@ struct CategoryHome: View
         {
             List
             {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets());
+                
                 ForEach(modelData.categories.keys.sorted(), id: \.self)
                 {   key in
                     
-                    Text(key);
+                    CategoryRow(categoryName: key,
+                                items: modelData.categories[key]!)
                 }
+                .listRowInsets(EdgeInsets());
             }
-                .navigationTitle("Featured");
+            .navigationTitle("Featured");
         }
     detail:
         {
