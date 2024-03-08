95d21cdac63261a8fe4077695097691b84ac91c8
Reckhammer <joshua.rechkemmer48@gmail.com>
2024-03-07 18:23:04 -0600

    Chapter 3 Part 1 Section 1, Created a Category home view

---
+++ Landmark/Views/Categories/CategoryHome.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryHome.swift
+//  Landmark
+//
+//  Created by Joshua Rechkemmer on 3/7/24.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryHome()
+}
