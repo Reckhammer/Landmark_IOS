a35445f1f7f034e35c2cee2c2de1d9b4cca19ff9
Reckhammer <joshua.rechkemmer48@gmail.com>
2024-03-20 17:34:57 -0500

    Chapter 3 Part 2 Section 3: Created a profile editor view

--- Landmark/Views/Profiles/ProfileHost.swift
+++ Landmark/Views/Profiles/ProfileHost.swift
@@ -29,7 +29,7 @@ struct ProfileHost: View
             }
             else
             {
-                Text("Profile Editor");
+                ProfileEditor(profile: $draftProfile);
             }
             
         }
